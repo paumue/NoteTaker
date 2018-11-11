c5a4dac96a50c6a30d0dfef5f77e08eb1cab28d0
paumue <paulhandball@gmail.com>
2018-11-11 02:48:26 +0000

    Implementing Voice to text

--- textchat.py
+++ textchat.py
@@ -1,22 +1,48 @@
-#give us number
-# we send them a sms
-# wait for response
-# parse the response to a voice message
-# voice message to call
-# keep call open
 
 import os
 from twilio.rest import Client
+from flask import Flask
+import lxml.etree as E
+import lxml.builder
 
+# Initializing environmental  variables
+
+#acount_sid= os.environ["twilio_account_sid"]
+#auth_token = os.environ["twilio_auth_token"]
 # Your Account SID from twilio.com/console
 account_sid = os.environ["TWILIO_ACCOUNT_SID"]
 # Your Auth Token from twilio.com/console
 auth_token  = os.environ["TWILIO_AUTH_TOKEN"]
+# Initializing Twilio and Flask variables
 
 client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-    to="+447444072109",
-    from_="+441233800083",
-    body="Hello from Python!")
-print(message.sid)
+app = Flask(__name__)
+
+
+#Get text
+
+#set text to textmessage
+
+#Generate XML from text
+
+def xml_gen(txtmsg):
+    #TWXL categories
+    RESPONSE = E.response
+    #TWXL Fields
+    SAY = E.say
+    #Generate TWXL
+    pagetwxl = RESPONSE(
+                        SAY(txtmsg, voice = "Alice")
+                        )
+    return pagetwxl
+
+#xmlfile = open(responsetwxl.xml,"w")
+#xml_gen(textmessage).write(xmlfile)
+
+@app.route("/xml", methods=['GET','POST'])
+def xmlpage():
+    return xml_gen("Hello there")
+
+if __name__ == "__main__":
+    app.run(debug=True)
